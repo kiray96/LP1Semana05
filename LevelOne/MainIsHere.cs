580a344a2e642772d4cc417752dfc07e7c791666
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-19 21:48:57 +0000

    Add method doc

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -4,6 +4,10 @@ namespace LevelOne
 {
     public class Program
     {
+        /// <summary>
+        /// This method prints the line "Hello LP1!"
+        /// </summary>
+        /// <param name="args">unused arguments</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
